9f55f120f8cca62174806e68952c7c6b0b918eb1
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2023-01-02 11:44:53 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {     	for(j=n-1;j>=i;j--)     	{     		printf(" "); 		}         for(j=1;j<=i*2-1;j++)         {             printf("%d",i);         }         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -5,9 +5,13 @@ int main()
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=n;j>=1;j--)
+    	for(j=n-1;j>=i;j--)
+    	{
+    		printf(" ");
+		}
+        for(j=1;j<=i*2-1;j++)
         {
-            printf("%d ",j);
+            printf("%d",i);
         }
         printf("
 ");
