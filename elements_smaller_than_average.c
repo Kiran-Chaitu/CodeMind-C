d68b6ead65f4733d17d1b0d499cc4371d11227ce
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2023-01-05 16:25:52 +0000

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,avg,sum=0,c=0;     for(i=0;i<n;i++)     {        scanf("%d",&a[i]);        sum=sum+a[i];     }     avg=sum/n;     for(i=0;i<n;i++)     {       if(a[i]<=avg)       {           c++;       }     }     printf("%d",c); }

--- elements_smaller_than_average.c
+++ elements_smaller_than_average.c
@@ -3,19 +3,19 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int a[n],i,sum=0,c=0,avg;
+    int a[n],i,avg,sum=0,c=0;
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
-        sum+=a[i];
+       scanf("%d",&a[i]);
+       sum=sum+a[i];
     }
     avg=sum/n;
     for(i=0;i<n;i++)
     {
-        if(a[i]<=avg)
-        {
-            c++;
-        }
+      if(a[i]<=avg)  
+      {
+          c++;
+      }
     }
     printf("%d",c);
 }
