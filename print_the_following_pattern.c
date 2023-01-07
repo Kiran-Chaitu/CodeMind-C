87f8838a90db51d3646ff78e602966b508c070e5
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2023-01-07 05:00:58 +0000

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<n;i++)     {         for(j=1;j<=i;j++)         {             if(i==n||i==j||j==1)                 printf("*");             else                 printf(" ");         }         printf(" ");     }     for(i=1;i<=n;i++)         printf("*");     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,14 +3,19 @@ int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=1;i<n;i++)
     {
-        for(j=1;j<=n;j++)
+        for(j=1;j<=i;j++)
         {
-            printf("%c ",i+64);
+            if(i==n||i==j||j==1)
+                printf("*");
+            else 
+                printf(" ");
         }
         printf("
 ");
     }
+    for(i=1;i<=n;i++)
+        printf("*");
     return 0;
 }
