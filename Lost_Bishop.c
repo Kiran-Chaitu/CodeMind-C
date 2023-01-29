eade01ebd3df1599952321a7b8a8b117e50bf850
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2023-01-30 05:04:48 +0530

    #include <stdio.h> int main() {     char ch[8][8];     int i,j,x,y,t,k;     scanf("%d",&t);     for(k=1;k<=t;k++)     {     	for(i=0;i<8;i++) 		{         	for (j=0;j<8;j++) 			{             	scanf(" %c",&ch[i][j]);         	}     	}     	for(i=0;i<8;i++) 		{         	for(j=0;j<8;j++) 			{             	if(ch[i][j]=='#') 				{             		if(ch[i-1][j-1]=='#'&&ch[i-1][j+1]=='#'&&ch[i+1][j-1]=='#'&&ch[i+1][j+1]=='#')             		{             			x=i;             			y=j; 					} 				}         	}     	}     	printf("%d %d ",x+1,y+1); 	}     return 0; }

--- Lost_Bishop.c
+++ Lost_Bishop.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
-int main() {
+int main() 
+{
     char ch[8][8];
-    int i,j,x,y,c=0,t,k;
+    int i,j,x,y,t,k;
     scanf("%d",&t);
     for(k=1;k<=t;k++)
     {
     	for(i=0;i<8;i++)
-	{
-        for (j=0;j<8;j++)
 		{
-            scanf(" %c",&ch[i][j]);
-        }
-    }
-    for(i=0;i<8;i++)
-	{
-        for(j=0;j<8;j++)
-		{
-            if(ch[i][j]=='#')
+        	for (j=0;j<8;j++)
 			{
-            	if(ch[i-1][j-1]=='#'&&ch[i-1][j+1]=='#'&&ch[i+1][j-1]=='#'&&ch[i][j]=='#')
-            	{
-            		c++;
-            		x=i;
-            		y=j;
+            	scanf(" %c",&ch[i][j]);
+        	}
+    	}
+    	for(i=0;i<8;i++)
+		{
+        	for(j=0;j<8;j++)
+			{
+            	if(ch[i][j]=='#')
+				{
+            		if(ch[i-1][j-1]=='#'&&ch[i-1][j+1]=='#'&&ch[i+1][j-1]=='#'&&ch[i+1][j+1]=='#')
+            		{
+            			x=i;
+            			y=j;
+					}
 				}
-			}
-        }
-    }
-    printf("%d %d
+        	}
+    	}
+    	printf("%d %d
 ",x+1,y+1);
 	}
     return 0;
