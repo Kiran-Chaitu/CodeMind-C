b6b4bdab3c1b421f7633679a93ddce8658541975
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2023-01-29 17:50:21 +0530

    #include <stdio.h> int main() {     char ch[8][8];     int i,j,x,y,c=0,t,k;     scanf("%d",&t);     for(k=1;k<=t;k++)     {     	for(i=0;i<8;i++) 	{         for (j=0;j<8;j++) 		{             scanf(" %c",&ch[i][j]);         }     }     for(i=0;i<8;i++) 	{         for(j=0;j<8;j++) 		{             if(ch[i][j]=='#') 			{             	if(ch[i-1][j-1]=='#'&&ch[i-1][j+1]=='#'&&ch[i+1][j-1]=='#'&&ch[i][j]=='#')             	{             		c++;             		x=i;             		y=j; 				} 			}         }     }     printf("%d %d ",x+1,y+1); 	}     return 0; }

--- Lost_Bishop.c
+++ Lost_Bishop.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
-#define R 8
-#define C 8
 int main() {
-    char ch[R][C];
-    int i,j,x,y,t,k;
+    char ch[8][8];
+    int i,j,x,y,c=0,t,k;
     scanf("%d",&t);
     for(k=1;k<=t;k++)
     {
-    	for(i=0;i<R;i++)
+    	for(i=0;i<8;i++)
 	{
-        for (j=0;j<C;j++)
+        for (j=0;j<8;j++)
 		{
             scanf(" %c",&ch[i][j]);
         }
     }
-    for(i=0;i<R;i++)
+    for(i=0;i<8;i++)
 	{
-        for(j=0;j<C;j++)
+        for(j=0;j<8;j++)
 		{
             if(ch[i][j]=='#')
 			{
             	if(ch[i-1][j-1]=='#'&&ch[i-1][j+1]=='#'&&ch[i+1][j-1]=='#'&&ch[i][j]=='#')
             	{
+            		c++;
             		x=i;
             		y=j;
 				}
