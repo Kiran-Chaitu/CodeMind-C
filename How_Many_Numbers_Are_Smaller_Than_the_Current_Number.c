e1f32f604ae47a896b896f18274014f2877e6fd7
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2023-03-29 11:06:24 +0530

    #include<stdio.h> int main() { 	int n,i,j,c=0; 	scanf("%d",&n); 	int a[n],d[n]; 	for(i=0;i<n;i++) 	{ 		scanf("%d",&a[i]); 	} 	for(i=0;i<n;i++) 	{ 		c=0; 		for(j=0;j<n;j++) 		{ 			if(a[i]>a[j]) 				c++; 		} 		d[i]=c; 	} 	for(i=0;i<n;i++) 	{ 		printf("%d ",d[i]); 	} 	return 0; }

---
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,c=0;
+	scanf("%d",&n);
+	int a[n],d[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{	
+		c=0;
+		for(j=0;j<n;j++)
+		{
+			if(a[i]>a[j])
+				c++;
+		}
+		d[i]=c;
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",d[i]);
+	}
+	return 0;
+}
