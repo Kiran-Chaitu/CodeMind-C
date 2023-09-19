c0cac4e1f8b3a16b060de7be897d0ef94ce1a869
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2023-09-19 15:15:23 +0530

    #include<stdio.h> int main(){
        int n;
        scanf("%d",&n);
        if(n==1) printf("1");
        else if(n==2) printf("2");
        else if(n==3) printf("4");
        else{
            int a=1,b=2,c=4,d;
            for(int i=4;i<=n;i++){
                d=a+b+c;
                a=b;
                b=c;
                c=d;
            }
            printf("%d",d);
        }
        
        return 0;
    }

---
+++ Count_number_of_ways_to_cover_a_distance.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    
+    int n;
+    scanf("%d",&n);
+    if(n==1) printf("1");
+    else if(n==2) printf("2");
+    else if(n==3) printf("4");
+    else{
+        int a=1,b=2,c=4,d;
+        for(int i=4;i<=n;i++){
+            d=a+b+c;
+            a=b;
+            b=c;
+            c=d;
+        }
+        printf("%d",d);
+    }
+    
+    return 0;
+}
